f54aa0d8a07d7bb570af94aee53407b2bddb8bfc
JKDrumm <jkdrumm1@gmail.com>
2020-02-14 17:39:56 -0700

    US: 17, Task: 35 - Created Register page for user to enter information. Also created IncompleteActivity to show when a feature is not yet implemented.

--- app/src/main/java/com/example/jaqb/MainActivity.java
+++ app/src/main/java/com/example/jaqb/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.jaqb;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.jaqb.ui.login.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void loginButtonOnClick(View view)
+    {
+        Intent intent = new Intent(this, IncompleteActivity.class);
+        startActivity(intent);
+    }
+
+    public void registerButtonOnClick(View view)
+    {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
