9081eeda648ba13961cced5a18d6ffd1d0853383
asing225 <amanjot5552@gmail.com>
2020-02-19 16:27:00 -0700

    37 - Adding database dependency to gradle

--- app/src/main/java/com/example/jaqb/MainActivity.java
+++ app/src/main/java/com/example/jaqb/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.jaqb;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button saveButton;
+    EditText name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        saveButton = (Button) findViewById(R.id.save);
+        name = (EditText) findViewById(R.id.name);
     }
 }
