248a68d8afa7b09059cd06a6861b162cd4d807f4
asing225 <amanjot5552@gmail.com>
2020-02-19 17:18:13 -0700

    37 - Tested database write to the firebase DB

--- app/src/main/java/com/example/jaqb/MainActivity.java
+++ app/src/main/java/com/example/jaqb/MainActivity.java
@@ -2,19 +2,37 @@ package com.example.jaqb;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.jaqb.model.User;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button saveButton;
+    Button btnSave;
     EditText name;
+    DatabaseReference reff;
+    User newUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        saveButton = (Button) findViewById(R.id.save);
+        btnSave = (Button) findViewById(R.id.save);
         name = (EditText) findViewById(R.id.name);
+        newUser = new User();
+        reff = FirebaseDatabase.getInstance().getReference().child("User");
+        btnSave.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                newUser.setUsername(name.getText().toString());
+                reff.push().setValue(newUser);
+                Toast.makeText(MainActivity.this, "User created", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/jaqb/model/User.java
@@ -0,0 +1,17 @@
+package com.example.jaqb.model;
+
+public class User {
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    private String username;
+
+    public User(){
+    }
+}
