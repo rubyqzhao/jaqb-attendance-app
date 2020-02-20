4270b46813ccff0f75fb2f90d99b5d11917c7e9e
asing225 <amanjot5552@gmail.com>
2020-02-19 18:03:00 -0700

    36 - Added model class for an unregistered user

--- app/src/main/java/com/example/jaqb/data/model/User.java
+++ app/src/main/java/com/example/jaqb/data/model/User.java
@@ -1,17 +1,49 @@
 package com.example.jaqb.data.model;
 
+/**
+ * @author amanjotsingh
+ * This class contains the attributes of a user entered at the time for registering for the app.
+ * */
+
 public class User {
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    private String username;
-
     public User(){
+    }
+
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
