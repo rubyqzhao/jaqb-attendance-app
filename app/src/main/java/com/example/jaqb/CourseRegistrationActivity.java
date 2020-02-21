cf70c64b1419c04e96bc23b4b01435e2df5e3dda
asing225 <amanjot5552@gmail.com>
2020-02-21 15:55:21 -0700

    44 - Added activity files for the register course page

---
+++ app/src/main/java/com/example/jaqb/CourseRegistrationActivity.java
@@ -0,0 +1,12 @@
+package com.example.jaqb;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CourseRegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
