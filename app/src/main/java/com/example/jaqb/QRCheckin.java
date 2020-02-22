68cdb24bfec067575e1bfc7c1ea69bc731edff00
Bharat Goel <bharat1mrt@gmail.com>
2020-02-22 11:58:30 -0700

    UI Added with text box and button, click goes to incomplete activity

--- app/src/main/java/com/example/jaqb/QRCheckin.java
+++ app/src/main/java/com/example/jaqb/QRCheckin.java
@@ -2,13 +2,28 @@ package com.example.jaqb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class QRCheckin extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcheckin);
+    }
+
+    public void qrButtonOnClick(View view) {
+        Intent intent = new Intent(QRCheckin.this, IncompleteActivity.class);
+        startActivity(intent);
+    }
+
+    public void codeButtonOnClick( View view) {
+        Intent intent = new Intent(QRCheckin.this, IncompleteActivity.class);
+        startActivity(intent);
     }
 }
