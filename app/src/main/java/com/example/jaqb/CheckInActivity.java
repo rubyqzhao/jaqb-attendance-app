a910cc0fa41db1c55ffd2eecf8f393d078573c4c
Ruby Zhao <rubyqzhao@gmail.com>
2020-02-23 10:58:05 -0700

    Added links to all buttons on checkin page, currently to incomplete (US 18, task 62)

--- app/src/main/java/com/example/jaqb/CheckInActivity.java
+++ app/src/main/java/com/example/jaqb/CheckInActivity.java
@@ -1,6 +1,11 @@
 package com.example.jaqb;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.jaqb.R;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +15,28 @@ public class CheckInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkin);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar2);
+        setSupportActionBar(myToolbar);
+    }
+
+    public void checkinButtonOnClick(View view) {
+        Intent intent = new Intent(this, IncompleteActivity.class);
+        startActivity(intent);
+    }
+
+    public void setAlarmButtonOnClick(View view) {
+        Intent intent = new Intent(this, IncompleteActivity.class);
+        startActivity(intent);
+    }
+
+    public void seeRewardsButtonOnClick(View view) {
+        Intent intent = new Intent(this, IncompleteActivity.class);
+        startActivity(intent);
+    }
+
+    public void seeAttendanceButtonOnClick(View view) {
+        Intent intent = new Intent(this, IncompleteActivity.class);
+        startActivity(intent);
     }
 
 }
