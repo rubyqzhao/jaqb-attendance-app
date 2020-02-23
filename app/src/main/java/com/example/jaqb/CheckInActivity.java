baba0b9db7d3d2023a58d8ce074970278aea0683
Ruby Zhao <rubyqzhao@gmail.com>
2020-02-23 12:01:24 -0700

    Connected logout button to logged-out screen (US 16, task 42)

--- app/src/main/java/com/example/jaqb/CheckInActivity.java
+++ app/src/main/java/com/example/jaqb/CheckInActivity.java
@@ -3,6 +3,8 @@ package com.example.jaqb;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -13,8 +15,14 @@ public class CheckInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkin);
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar2);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.checkin_toolbar);
         setSupportActionBar(myToolbar);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar, menu);
+        return true;
     }
 
     public void checkinButtonOnClick(View view) {
@@ -34,6 +42,12 @@ public class CheckInActivity extends AppCompatActivity {
 
     public void seeAttendanceButtonOnClick(View view) {
         Intent intent = new Intent(this, IncompleteActivity.class);
+        startActivity(intent);
+    }
+
+    public void logoutButtonOnClick(View view) {
+        //todo: remove session information
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 
