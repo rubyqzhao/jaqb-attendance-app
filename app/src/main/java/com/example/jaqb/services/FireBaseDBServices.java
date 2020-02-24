20eeb2e9fd2d845242b932aa6740a98defe18e6f
asing225 <amanjot5552@gmail.com>
2020-02-24 14:44:14 -0700

    Removed unused import statements

--- app/src/main/java/com/example/jaqb/services/FireBaseDBServices.java
+++ app/src/main/java/com/example/jaqb/services/FireBaseDBServices.java
@@ -1,7 +1,6 @@
 package com.example.jaqb.services;
 
 import android.content.Context;
-import android.content.Intent;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -17,22 +16,13 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import android.view.View;
 
-import androidx.annotation.NonNull;
-
-import com.example.jaqb.R;
-import com.example.jaqb.data.model.Course;
 import com.example.jaqb.data.model.User;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.Observer;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author amanjotsinghs
