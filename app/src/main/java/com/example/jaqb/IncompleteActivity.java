e6c453411817e2dad796fa46e8f24bb6bef625da
Bharat Goel <bharat1mrt@gmail.com>
2020-02-24 21:17:39 -0700

    Errors fixed with Intent

--- app/src/main/java/com/example/jaqb/IncompleteActivity.java
+++ app/src/main/java/com/example/jaqb/IncompleteActivity.java
@@ -8,16 +8,17 @@ import android.widget.TextView;
 
 public class IncompleteActivity extends AppCompatActivity {
 
-    TextView textView = findViewById(R.id.testTextView);
+    TextView testTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_incomplete);
 
+        testTextView = findViewById(R.id.testTextView);
         //Test Code
         Intent intent = getIntent();
         String str = intent.getStringExtra("data");
-        textView.setText(str);
+        testTextView.setText(str);
     }
 }
--- app/src/main/java/com/example/jaqb/QRCheckin.java
+++ app/src/main/java/com/example/jaqb/QRCheckin.java
@@ -1,22 +1,25 @@
 package com.example.jaqb;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+//import com.droidmentor.locationhelper.LocationUtil.LocationHelper;
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.barcode.Barcode;
@@ -32,6 +35,10 @@ public class QRCheckin extends AppCompatActivity {
     TextView textView;
     Boolean value = false;
     EditText editText;
+    Location lastLocation;
+    double Long;
+    double Lat;
+    //LocationHelper locationHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -96,7 +103,7 @@ public class QRCheckin extends AppCompatActivity {
                                 value = true;
 
                             Intent intent = new Intent(QRCheckin.this, IncompleteActivity.class);
-                            intent.putExtra("success", value);
+                            //intent.putExtra("success", value);
                             intent.putExtra("data", qrCodes.valueAt(0).displayValue);
                             startActivity(intent);
                         }
@@ -111,8 +118,8 @@ public class QRCheckin extends AppCompatActivity {
         if(checkValues() == true)
             value = true;
         Intent intent = new Intent(QRCheckin.this, IncompleteActivity.class);
-        intent.putExtra("success", value);
-        intent.putExtra("data", editText.getText());
+        //intent.putExtra("success", value);
+        intent.putExtra("data", editText.getText().toString());
         startActivity(intent);
     }
 
@@ -122,5 +129,14 @@ public class QRCheckin extends AppCompatActivity {
         double val = Math.random()*2;
         if(val > 1) return true;
         return false;
+    }
+
+    void getLocation() {
+        try {
+            //locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            //locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, (LocationListener) this);
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        }
     }
 }
