66024d76d2dd3cb22396ff50ce9437474212ec3f
Ruby Zhao <rubyqzhao@gmail.com>
2020-02-27 13:19:51 -0700

    Added location tagging to instructor page (US23, t59)

--- app/src/main/java/com/example/jaqb/ui/instructor/HomeActivity.java
+++ app/src/main/java/com/example/jaqb/ui/instructor/HomeActivity.java
@@ -1,10 +1,12 @@
 package com.example.jaqb.ui.instructor;
 
 import android.content.Intent;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -14,13 +16,29 @@ import com.example.jaqb.IncompleteActivity;
 import com.example.jaqb.MainActivity;
 import com.example.jaqb.R;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 public class HomeActivity extends AppCompatActivity {
+    private FusedLocationProviderClient fusedLocationClient;
+    private double latitude;
+    private double longitude;
+    private TextView coordDisplay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructor_classes);
         Toolbar myToolbar = findViewById(R.id.instructor_toolbar);
         setSupportActionBar(myToolbar);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        coordDisplay = findViewById(R.id.gps_coord);
+    }
+
+    protected void onResume() {
+        super.onResume();
+        //todo: update coordDisplay using what's stored in the database
     }
 
     @Override
@@ -56,5 +74,19 @@ public class HomeActivity extends AppCompatActivity {
     public void GetQRButtonOnClick(View view) {
         Intent intent = new Intent(this, IncompleteActivity.class);
         startActivity(intent);
+    }
+
+    public void submitLocationButtonOnClick(View view) {
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                            coordDisplay.setText(latitude + ", " + longitude);
+                        }
+                    }
+                });
     }
 }
