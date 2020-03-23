5956716be652f376e5f40b508dcf80f331c17395
asing225 <amanjot5552@gmail.com>
2020-03-23 10:48:12 -0700

    112, 113 - Added activity for displaying the QR code for the istructor

---
+++ app/src/main/java/com/example/jaqb/ui/instructor/DisplayQRCodeActivity.java
@@ -0,0 +1,15 @@
+package com.example.jaqb.ui.instructor;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.jaqb.R;
+
+public class DisplayQRCodeActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_qr_code);
+        // display the QR code on the screen for the instructor
+    }
+}
