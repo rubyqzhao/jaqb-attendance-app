81fd791cb7052f64fc4112ae467852b37c84c463
asing225 <amanjot5552@gmail.com>
2020-03-26 22:03:43 -0700

    134 - Changed the student Ids to names in show attendance history

--- app/src/main/java/com/example/jaqb/ui/instructor/CheckAttendance.java
+++ app/src/main/java/com/example/jaqb/ui/instructor/CheckAttendance.java
@@ -7,23 +7,33 @@ import android.widget.ListView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.jaqb.R;
+import com.example.jaqb.data.model.RegisteredUser;
+import com.example.jaqb.data.model.User;
+import com.example.jaqb.services.FireBaseDBServices;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CheckAttendance extends AppCompatActivity {
 
     private String courseCode;
     private String date;
-    private List<String> studentsAttendance;
+    private List<RegisteredUser> studentsAttendance;
+    private List<String> studentIds;
+    private List<String> studentPresence;
+    private List<String> studentNames;
+    private Map<String, RegisteredUser> studentData;
     private ListView listView;
     private ArrayAdapter<String> arrayAdapter;
     private DatabaseReference databaseReference;
-    private String[] name = new String[2];
+    private DatabaseReference databaseReference2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,18 +43,45 @@ public class CheckAttendance extends AppCompatActivity {
         date = (String) getIntent().getCharSequenceExtra("date");
         databaseReference = FirebaseDatabase.getInstance().getReference("InstructorAttendance")
                 .child(courseCode).child(date);
-        studentsAttendance = new ArrayList<>();
+        databaseReference2 = FirebaseDatabase.getInstance().getReference("User");
+        studentsAttendance = new ArrayList<RegisteredUser>();
+        studentIds = new ArrayList<String>();
+        studentPresence = new ArrayList<String>();
+        studentNames = new ArrayList<String>();
+        studentData = new HashMap<String, RegisteredUser>();
         listView = (ListView) findViewById(R.id.dates_course_list);
         findViewById(R.id.dates_progressBar).setVisibility(View.GONE);
-        arrayAdapter = new ArrayAdapter<>(this, R.layout.class_list_item, R.id.class_item, studentsAttendance);
+        arrayAdapter = new ArrayAdapter<>(this, R.layout.class_list_item, R.id.class_item_name, studentNames);
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for(DataSnapshot keyNode : dataSnapshot.getChildren()){
+                for(DataSnapshot keyNode : dataSnapshot.getChildren()) {
                     String studentId = keyNode.getKey();
                     boolean attendance = (boolean) keyNode.getValue();
-                    studentsAttendance.add(studentId + " : " + ((attendance)?"Present" : "Absent"));
+                    studentIds.add(studentId);
+                    studentPresence.add((attendance) ? "Present" : "Absent");
                 }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        databaseReference2.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for(DataSnapshot keyNode : dataSnapshot.getChildren()){
+                    String key = keyNode.getKey();
+                    String fname = (String) keyNode.child("fname").getValue();
+                    String lname = (String) keyNode.child("lname").getValue();
+//                    System.out.println("THIS IS TEH VALUE : " + keyNode.getValue());
+                    System.out.println("THIS IS FIRST NAME : " + keyNode.child("fname").getValue());
+                    System.out.println("THIS IS LAST NAME : " + keyNode.child("lname").getValue());
+                    RegisteredUser user = new RegisteredUser(fname, lname);
+                    studentData.put(key, user);
+                }
+                getDisplayDate();
                 listView.setAdapter(arrayAdapter);
             }
 
@@ -53,5 +90,23 @@ public class CheckAttendance extends AppCompatActivity {
 
             }
         });
+
+        for(Map.Entry<String, RegisteredUser> entry : studentData.entrySet()){
+            System.out.println("READING MAP KEY INSIDE : " + entry.getKey());
+            System.out.println("READING MAP VALUE INSIDE : " + entry.getValue());
+        }
+    }
+
+    private void getDisplayDate(){
+        for(String id : studentIds){
+            if(studentData.containsKey(id)){
+                studentsAttendance.add(studentData.get(id));
+            }
+        }
+        for(int i=0; i< studentsAttendance.size(); i++){
+            RegisteredUser user = studentsAttendance.get(i);
+            String presence = studentPresence.get(i);
+            studentNames.add(user.getfName() + " " + user.getlName() + " : " + presence);
+        }
     }
 }
--- app/src/main/java/com/example/jaqb/ui/instructor/ClassDatesActivity.java
+++ app/src/main/java/com/example/jaqb/ui/instructor/ClassDatesActivity.java
@@ -9,12 +9,9 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.jaqb.R;
-import com.example.jaqb.services.FireBaseDBServices;
-import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -43,7 +40,7 @@ public class ClassDatesActivity extends AppCompatActivity implements AdapterView
         courseDates = new ArrayList<>();
         listView = (ListView) findViewById(R.id.dates_course_list);
         findViewById(R.id.dates_progressBar).setVisibility(View.GONE);
-        arrayAdapter = new ArrayAdapter<>(this, R.layout.class_list_item, R.id.class_item, courseDates);
+        arrayAdapter = new ArrayAdapter<>(this, R.layout.class_list_item, R.id.class_item_name, courseDates);
         databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
