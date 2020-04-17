4f004d594f537ff9aa381abe905e5847b50eb5c5
asing225 <amanjot5552@gmail.com>
2020-04-16 19:49:50 -0700

    216 - Fix for data not showing up if reopened the activity

--- app/src/main/java/com/example/jaqb/ui/instructor/AttendanceHistoryByNamesActivity.java
+++ app/src/main/java/com/example/jaqb/ui/instructor/AttendanceHistoryByNamesActivity.java
@@ -6,13 +6,9 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.jaqb.CourseDetailsActivity;
 import com.example.jaqb.R;
-import com.example.jaqb.data.model.Course;
 import com.example.jaqb.data.model.RegisteredUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -20,7 +16,6 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -28,39 +23,45 @@ import java.util.Map;
 
 public class AttendanceHistoryByNamesActivity extends AppCompatActivity {
 
+    private String courseCode;
+    private String date;
     private List<RegisteredUser> studentsAttendance;
     private List<String> studentIds;
+    private List<String> studentPresence;
     private List<String> studentNames;
     private Map<String, RegisteredUser> studentData;
     private ListView listView;
     private ArrayAdapter<String> arrayAdapter;
-    private DatabaseReference databaseReference;
-    private String courseCode;
+    private Query databaseReference;
+    private DatabaseReference databaseReference2;
 
+    /**
+     * Initial method that triggers when the user accesses the attendance list
+     * for the first time in the app. Creates the view and initializes values
+     * using data pulled from firebase.
+     * @param savedInstanceState    the previous state of the app
+     */
     @Override
-    public void onCreate(Bundle savedInstance) {
-        super.onCreate(savedInstance);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_class_dates);
         courseCode = (String) getIntent().getCharSequenceExtra("code");
-
-        databaseReference = FirebaseDatabase.getInstance().getReference("User");
+        date = (String) getIntent().getCharSequenceExtra("date");
+        databaseReference = FirebaseDatabase.getInstance().getReference("Course")
+                .orderByChild("code").equalTo(courseCode);
+        databaseReference2 = FirebaseDatabase.getInstance().getReference("User");
         studentsAttendance = new ArrayList<RegisteredUser>();
         studentIds = new ArrayList<String>();
+        studentPresence = new ArrayList<String>();
         studentNames = new ArrayList<String>();
         studentData = new HashMap<String, RegisteredUser>();
         listView = (ListView) findViewById(R.id.dates_course_list);
         findViewById(R.id.dates_progressBar).setVisibility(View.GONE);
         arrayAdapter = new ArrayAdapter<>(this, R.layout.class_list_item, R.id.class_item_name, studentNames);
-
-
-        Query query = FirebaseDatabase.getInstance().getReference("Course").orderByChild("code")
-                .equalTo(courseCode);
-
-        query.addListenerForSingleValueEvent(new ValueEventListener() {
+        databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for(DataSnapshot keyNode : dataSnapshot.getChildren()){
-                    String key = keyNode.getKey();
+                for(DataSnapshot keyNode : dataSnapshot.getChildren()) {
                     for(DataSnapshot node : keyNode.getChildren()){
                         if(node.getKey().equalsIgnoreCase("students")){
                             for(DataSnapshot shots : node.getChildren()){
@@ -68,7 +69,6 @@ public class AttendanceHistoryByNamesActivity extends AppCompatActivity {
                             }
                         }
                     }
-
                 }
             }
 
@@ -77,8 +77,7 @@ public class AttendanceHistoryByNamesActivity extends AppCompatActivity {
 
             }
         });
-
-        databaseReference.addValueEventListener(new ValueEventListener() {
+        databaseReference2.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 for(DataSnapshot keyNode : dataSnapshot.getChildren()){
@@ -102,9 +101,9 @@ public class AttendanceHistoryByNamesActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String item = arrayAdapter.getItem(position);
-                String item_id = item.split(":")[1].trim();
+//                String item_id = item.split(":")[1].trim();
                 Intent intent = new Intent(getApplicationContext(), AttendanceHistoryIndividualStudentActivity.class);
-                intent.putExtra("studentId", item_id.substring(0, item_id.length() - 1));
+//                intent.putExtra("studentId", item_id.substring(0, item_id.length() - 1));
                 intent.putExtra("code", courseCode);
                 startActivity(intent);
             }
@@ -114,13 +113,12 @@ public class AttendanceHistoryByNamesActivity extends AppCompatActivity {
     private void getDisplayDate(){
         for(String id : studentIds){
             if(studentData.containsKey(id)){
-                RegisteredUser user = studentData.get(id);
-                studentNames.add(user.getfName() + " " + user.getlName() + " (Id : " + id + ")");
+                studentsAttendance.add(studentData.get(id));
             }
         }
-//        for(int i=0; i< studentsAttendance.size(); i++){
-//            RegisteredUser user = studentsAttendance.get(i);
-//            studentNames.add(user.getfName() + " " + user.getlName());
-//        }
+        for(int i=0; i< studentsAttendance.size(); i++){
+            RegisteredUser user = studentsAttendance.get(i);
+            studentNames.add(user.getfName() + " " + user.getlName());
+        }
     }
 }
