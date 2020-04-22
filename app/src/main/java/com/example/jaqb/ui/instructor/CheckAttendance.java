3ab56aefe22c046d3f001e0425e21b9afdebf899
asing225 <amanjot5552@gmail.com>
2020-04-22 01:22:48 -0700

    Added pie chart for individual student attendance

--- app/src/main/java/com/example/jaqb/ui/instructor/CheckAttendance.java
+++ app/src/main/java/com/example/jaqb/ui/instructor/CheckAttendance.java
@@ -14,6 +14,11 @@ import com.example.jaqb.R;
 import com.example.jaqb.data.model.RegisteredUser;
 import com.example.jaqb.data.model.User;
 import com.example.jaqb.services.FireBaseDBServices;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -130,6 +135,39 @@ public class CheckAttendance extends AppCompatActivity {
 
             }
         });
+
+        PieChart pieChart = findViewById(R.id.piechart);
+        pieChart.setVisibility(View.VISIBLE);
+        ArrayList NoOfEmp = new ArrayList();
+
+        NoOfEmp.add(new Entry(945f, 0));
+        NoOfEmp.add(new Entry(1040f, 1));
+        NoOfEmp.add(new Entry(1133f, 2));
+        NoOfEmp.add(new Entry(1240f, 3));
+        NoOfEmp.add(new Entry(1369f, 4));
+        NoOfEmp.add(new Entry(1487f, 5));
+        NoOfEmp.add(new Entry(1501f, 6));
+        NoOfEmp.add(new Entry(1645f, 7));
+        NoOfEmp.add(new Entry(1578f, 8));
+        NoOfEmp.add(new Entry(1695f, 9));
+        PieDataSet dataSet = new PieDataSet(NoOfEmp, "Number Of Employees");
+
+        ArrayList year = new ArrayList();
+
+        year.add("2008");
+        year.add("2009");
+        year.add("2010");
+        year.add("2011");
+        year.add("2012");
+        year.add("2013");
+        year.add("2014");
+        year.add("2015");
+        year.add("2016");
+        year.add("2017");
+        PieData data = new PieData(year, dataSet);
+        pieChart.setData(data);
+        dataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        pieChart.animateXY(5000, 5000);
     }
 
     private void getDisplayDate(){
